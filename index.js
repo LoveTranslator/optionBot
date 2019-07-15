d82054b9aafc7e2f5646b05c84def155ac5cfacb
Александр Морозов <iamaleks.morozov@gmail.com>
2019-07-15 18:20:40 +0300

    new id

--- index.js
+++ index.js
@@ -10,55 +10,71 @@ var bot = new TelegramBot(token, { polling: true });
 
 var user = [];
 
+function test() {
+    console.log('123');
+}
+
+/* var menu = {
+    reply_markup: JSON.stringify({
+        keyboard: [
+            [{
+                text: 'Перезапустить опрос',
+                callback_data: test()
+            }]
+        ],
+        resize_keyboard: true,
+        one_time_keyboard: true
+    })
+}; */
 
 var questions = [{
-        title: 'Вашему аккаунту больше года?',
-        buttons: [
-            [{ text: 'Да', callback_data: '0_1' }],
-            [{ text: 'Нет', callback_data: '0_2' }],
-        ],
-        right_answer: 1
-    },
-    {
-        title: 'К Вашему аккаунту привязана действующая почта и мобильный телефон?',
-        buttons: [
-            [{ text: 'Да', callback_data: '1_1' }],
-            [{ text: 'Нет', callback_data: '1_2' }],
-        ],
-        right_answer: 1
-    },
-    {
-        title: 'Ваш аккаунт заполнен (содержит информацию об учебе или работе, есть фото, друзья, записи на стене)?',
-        buttons: [
-            [{ text: 'Да', callback_data: '2_1' }],
-            [{ text: 'Нет', callback_data: '2_2' }],
-        ],
-        right_answer: 1
-    },
-    {
-        title: 'Вы проявляли социальную активность(отметки "нравится", комментарии, прохождение моментальных игр, репосты записей сообществ на свою страницу) на аккаунте в течении всего его существования?',
-        buttons: [
-            [{ text: 'Да', callback_data: '3_1' }],
-            [{ text: 'Нет', callback_data: '3_2' }],
-        ],
-        right_answer: 1
-    },
-    {
-        title: 'Есть ли у Вас возможность проявлять социальную активность(отметки "нравится", комментарии, прохождение моментальных игр, репосты записей сообществ на свою страницу) с мобильного устройства во время аренды аккаунта?',
-        buttons: [
-            [{ text: 'Да', callback_data: '4_1' }],
-            [{ text: 'Нет', callback_data: '4_2' }],
-        ],
-        right_answer: 1
-    },
-    {
-        title: 'Согласны ли Вы на изменение пароля во время аренды аккаунта? (Это делается по техническим причинам, так как Facebook периодически просит о смене пароля для безопасности; новый пароль будет согласован с Вами)',
-        buttons: [
-            [{ text: 'Да', callback_data: '5_1' }],
-            [{ text: 'Нет', callback_data: '5_2' }],
-        ],
-        right_answer: 1
-    },
+    title: 'Вашему аккаунту больше года?',
+    buttons: [
+        [{ text: 'Да', callback_data: '0_1' }],
+        [{ text: 'Нет', callback_data: '0_2' }],
+    ],
+    right_answer: 1
+},
+{
+    title: 'К Вашему аккаунту привязана действующая почта и мобильный телефон?',
+    buttons: [
+        [{ text: 'Да', callback_data: '1_1' }],
+        [{ text: 'Нет', callback_data: '1_2' }],
+    ],
+    right_answer: 1
+},
+{
+    title: 'Ваш аккаунт заполнен (содержит информацию об учебе или работе, есть фото, друзья, записи на стене)?',
+    buttons: [
+        [{ text: 'Да', callback_data: '2_1' }],
+        [{ text: 'Нет', callback_data: '2_2' }],
+    ],
+    right_answer: 1
+},
+{
+    title: 'Вы проявляли социальную активность(отметки "нравится", комментарии, прохождение моментальных игр, репосты записей сообществ на свою страницу) на аккаунте в течении всего его существования?',
+    buttons: [
+        [{ text: 'Да', callback_data: '3_1' }],
+        [{ text: 'Нет', callback_data: '3_2' }],
+    ],
+    right_answer: 1
+},
+{
+    title: 'Есть ли у Вас возможность проявлять социальную активность(отметки "нравится", комментарии, прохождение моментальных игр, репосты записей сообществ на свою страницу) с мобильного устройства во время аренды аккаунта?',
+    buttons: [
+        [{ text: 'Да', callback_data: '4_1' }],
+        [{ text: 'Нет', callback_data: '4_2' }],
+    ],
+    right_answer: 1
+},
+{
+    title: 'Согласны ли Вы на изменение пароля во время аренды аккаунта? (Это делается по техническим причинам, так как Facebook периодически просит о смене пароля для безопасности; новый пароль будет согласован с Вами)',
+    buttons: [
+        [{ text: 'Да', callback_data: '5_1' }],
+        [{ text: 'Нет', callback_data: '5_2' }],
+    ],
+    right_answer: 1
+},
 ];
 
 function newQuestion(msg) {
@@ -82,10 +98,11 @@ function newQuestion(msg) {
             user[i].answerNumber++;
         }
     }
+    
     bot.sendMessage(chat, text, options);
 }
 
-bot.onText(/\/start/, function(msg, match) {
+bot.onText(/\/start/, function (msg, match) {
     user.push({ id: msg.from.id, answerNumber: 0, countRightAnswer: 0 })
     bot.sendMessage(msg.from.id, `Мы – команда молодых SMM-специалистов. Недавно мы начали развивать направление Facebook Ads и столкнулись с проблемой ограниченных рекламных возможностей в этой социальной сети. К сожалению, Facebook запрещает создавать рекламные объявления со свежих аккаунтов, а также ограничивает создание объявлений с одного аккаунта. В связи с этим мы проделываем следующие этапы:
 
@@ -99,11 +116,12 @@ bot.onText(/\/start/, function(msg, match) {
   
 Пройдите небольшой опрос, чтобы мы могли узнать больше о Вашем аккаунте: 
   `);
-    /* console.log(user); */
+
+
     setTimeout(newQuestion.bind(this, msg), 2000);
 });
 
-bot.on('callback_query', function(msg) {
+bot.on('callback_query', function (msg) {
     var answer = msg.data.split('_');
     var index = answer[0]; // номер вопроса
     var button = answer[1]; // кнопка ответа 0 || 1
@@ -119,14 +137,14 @@ bot.on('callback_query', function(msg) {
             endPoll(msg);
         }
     }
-    /* console.log(user); */
+    console.log(user);
     newQuestion(msg);
 });
 
 function endPoll(msg) {
     for (let i = 0; i < user.length; i++) {
         if (user[i].id === msg.from.id && user[i].countRightAnswer === questions.length) {
-            bot.sendMessage(230431843, `Ёу хокаге, у нас новый генин, он успешно прошёл экзамен - @${msg.from.username}`); //230431843 - hokage
+            bot.sendMessage(841422237, `Ёу хокаге, у нас новый генин, он успешно прошёл экзамен - @${msg.from.username}`); //230431843 - hokage, newHokage - 841422237
             bot.sendMessage(msg.from.id, "Спасибо, наш менеджер скоро с вами свяжется:)");
             user.splice(i, 1);
             break;
