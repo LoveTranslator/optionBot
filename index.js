422e80971a78147bcf7c277d5cc1f43f68b976fa
Александр Морозов <iamaleks.morozov@gmail.com>
2019-09-03 16:18:07 +0300

    add send login instagram

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ process.env.NTBA_FIX_319 = 1
 var TelegramBot = require('node-telegram-bot-api');
 
 // Устанавливаем токен, который выдавал нам бот.
-var token = '766599402:AAGMExKVovquBJq8QA0RDkVqq2yQT7G4PmI'; // 766599402:AAGMExKVovquBJq8QA0RDkVqq2yQT7G4PmI
+
+var token = '922777706:AAGQU6l-8l2UMJ9qRVaQRyo59gQAzHGxrik'; // 933664978:AAG1_0xHSTk0wBVUERvHpK6z5z363WynyqE
 
 // Включить опрос сервера
-var bot = new TelegramBot(token, { polling: true });
+var bot = new TelegramBot(token, {
+    polling: true
+});
 
 var user = [];
+
+user.nick = undefined;
 
 var menu = {
     reply_markup: JSON.stringify({
@@ -23,53 +28,89 @@ var menu = {
 };
 
 var questions = [{
-    title: 'Вашему аккаунту больше года?',
-    buttons: [
-        [{ text: 'Да', callback_data: '0_1' }],
-        [{ text: 'Нет', callback_data: '0_2' }],
-    ],
-    right_answer: 1
-},
-{
-    title: 'К Вашему аккаунту привязана действующая почта и мобильный телефон?',
-    buttons: [
-        [{ text: 'Да', callback_data: '1_1' }],
-        [{ text: 'Нет', callback_data: '1_2' }],
-    ],
-    right_answer: 1
-},
-{
-    title: 'Ваш аккаунт заполнен (содержит информацию об учебе или работе, есть фото, друзья, записи на стене)?',
-    buttons: [
-        [{ text: 'Да', callback_data: '2_1' }],
-        [{ text: 'Нет', callback_data: '2_2' }],
-    ],
-    right_answer: 1
-},
-{
-    title: 'Вы проявляли социальную активность(отметки "нравится", комментарии, прохождение моментальных игр, репосты записей сообществ на свою страницу) на аккаунте в течении всего его существования?',
-    buttons: [
-        [{ text: 'Да', callback_data: '3_1' }],
-        [{ text: 'Нет', callback_data: '3_2' }],
-    ],
-    right_answer: 1
-},
-{
-    title: 'Есть ли у Вас возможность проявлять социальную активность(отметки "нравится", комментарии, прохождение моментальных игр, репосты записей сообществ на свою страницу) с мобильного устройства во время аренды аккаунта?',
-    buttons: [
-        [{ text: 'Да', callback_data: '4_1' }],
-        [{ text: 'Нет', callback_data: '4_2' }],
-    ],
-    right_answer: 1
-},
-{
-    title: 'Согласны ли Вы на изменение пароля во время аренды аккаунта? (Это делается по техническим причинам, так как Facebook периодически просит о смене пароля для безопасности; новый пароль будет согласован с Вами)',
-    buttons: [
-        [{ text: 'Да', callback_data: '5_1' }],
-        [{ text: 'Нет', callback_data: '5_2' }],
-    ],
-    right_answer: 1
-},
+        title: 'Вашему аккаунту больше года?',
+        buttons: [
+            [{
+                text: 'Да',
+                callback_data: '0_1'
+            }],
+            [{
+                text: 'Нет',
+                callback_data: '0_2'
+            }],
+        ],
+        right_answer: 1
+    },
+    {
+        title: 'К Вашему аккаунту привязана действующая почта и мобильный телефон?',
+        buttons: [
+            [{
+                text: 'Да',
+                callback_data: '1_1'
+            }],
+            [{
+                text: 'Нет',
+                callback_data: '1_2'
+            }],
+        ],
+        right_answer: 1
+    },
+    {
+        title: 'Ваш аккаунт заполнен (содержит информацию об учебе или работе, есть фото, друзья, записи на стене)?',
+        buttons: [
+            [{
+                text: 'Да',
+                callback_data: '2_1'
+            }],
+            [{
+                text: 'Нет',
+                callback_data: '2_2'
+            }],
+        ],
+        right_answer: 1
+    },
+    {
+        title: 'Вы проявляли социальную активность(отметки "нравится", комментарии, прохождение моментальных игр, репосты записей сообществ на свою страницу) на аккаунте в течении всего его существования?',
+        buttons: [
+            [{
+                text: 'Да',
+                callback_data: '3_1'
+            }],
+            [{
+                text: 'Нет',
+                callback_data: '3_2'
+            }],
+        ],
+        right_answer: 1
+    },
+    {
+        title: 'Есть ли у Вас возможность проявлять социальную активность(отметки "нравится", комментарии, прохождение моментальных игр, репосты записей сообществ на свою страницу) с мобильного устройства во время аренды аккаунта?',
+        buttons: [
+            [{
+                text: 'Да',
+                callback_data: '4_1'
+            }],
+            [{
+                text: 'Нет',
+                callback_data: '4_2'
+            }],
+        ],
+        right_answer: 1
+    },
+    {
+        title: 'Согласны ли Вы на изменение пароля во время аренды аккаунта? (Это делается по техническим причинам, так как Facebook периодически просит о смене пароля для безопасности; новый пароль будет согласован с Вами)',
+        buttons: [
+            [{
+                text: 'Да',
+                callback_data: '5_1'
+            }],
+            [{
+                text: 'Нет',
+                callback_data: '5_2'
+            }],
+        ],
+        right_answer: 1
+    }
 ];
 
 function newQuestion(msg) {
@@ -96,6 +137,52 @@ function newQuestion(msg) {
     bot.sendMessage(chat, text, options);
 }
 
+bot.on('callback_query', function onCallbackQuery(msg) {
+    var answer = msg.data.split('_');
+    var index = answer[0]; // номер вопроса
+    var button = answer[1]; // кнопка ответа 0 || 1
+    if (questions[index].right_answer == button) {
+        for (let i = 0; i < user.length; i++) {
+            if (user[i].id === msg.from.id) {
+                user[i].countRightAnswer++;
+            }
+        }
+    }
+
+    for (let i = 0; i < user.length; i++) {
+        if (user[i].id === msg.from.id && user[i].answerNumber === questions.length) {
+            endPoll(msg);
+        }
+    }
+
+    newQuestion(msg);
+});
+
+bot.on('message', function (msg) {
+    for (let i = 0; i < user.length; i++) {
+        if (user[i].id === msg.from.id && user[i].countRightAnswer === questions.length) {
+            user.instaLogin = msg.text;
+            bot.sendMessage(msg.from.id, "Отлично, скоро с вами свяжутся:)");
+            bot.sendMessage(841422237, `Ёу хокаге, у нас новый генин, он успешно прошёл экзамен - @${msg.from.username}.(id ${msg.from.id}). Instagram login - ${user.instaLogin}`); //230431843 - hokage, newHokage - 841422237
+            user.splice(i, 1);
+            break;
+        }
+    }
+
+});
+
+function endPoll(msg) {
+    for (let i = 0; i < user.length; i++) {
+        if (user[i].id === msg.from.id && user[i].countRightAnswer === questions.length) {
+            bot.sendMessage(msg.from.id, "Спасибо за пройденный опрос, оставьте ваше имя пользователя от аккаунта Instagram, и наш менеджер скоро с вами свяжется:)");
+        } else {
+            bot.sendMessage(msg.from.id, "К сожалению вы нам не подходите, попробуйте в следующий раз.");
+            user.splice(i, 1);
+            break;
+        }
+    }
+}
+
 bot.onText(/\/start/, function (msg, match) {
     myStart(msg);
 });
@@ -111,7 +198,13 @@ function myStart(msg) {
         }
     }
 
-    user.push({ id: msg.from.id, answerNumber: 0, countRightAnswer: 0 })
+    user.push({
+        id: msg.from.id,
+        answerNumber: 0,
+        countRightAnswer: 0,
+        answerNumberWithAccount: 0,
+        instaLogin: null
+    })
     bot.sendMessage(msg.from.id, `Мы – команда молодых SMM-специалистов. Недавно мы начали развивать направление Facebook Ads и столкнулись с проблемой ограниченных рекламных возможностей в этой социальной сети. К сожалению, Facebook запрещает создавать рекламные объявления со свежих аккаунтов, а также ограничивает создание объявлений с одного аккаунта. В связи с этим мы проделываем следующие этапы:
 
   1. арендуем аккаунты у людей 
@@ -125,39 +218,4 @@ function myStart(msg) {
 Пройдите небольшой опрос, чтобы мы могли узнать больше о Вашем аккаунте: 
   `, menu);
     setTimeout(newQuestion.bind(this, msg), 2000);
-}
-
-bot.on('callback_query', function (msg) {
-    var answer = msg.data.split('_');
-    var index = answer[0]; // номер вопроса
-    var button = answer[1]; // кнопка ответа 0 || 1
-    if (questions[index].right_answer == button) {
-        for (let i = 0; i < user.length; i++) {
-            if (user[i].id === msg.from.id) {
-                user[i].countRightAnswer++;
-            }
-        }
-    }
-    for (let i = 0; i < user.length; i++) {
-        if (user[i].id === msg.from.id && user[i].answerNumber === questions.length) {
-            endPoll(msg);
-        }
-    }
-    console.log(user);
-    newQuestion(msg);
-});
-
-function endPoll(msg) {
-    for (let i = 0; i < user.length; i++) {
-        if (user[i].id === msg.from.id && user[i].countRightAnswer === questions.length) {
-            bot.sendMessage(841422237, `Ёу хокаге, у нас новый генин, он успешно прошёл экзамен - @${msg.from.username}.(id ${msg.from.id})`); //230431843 - hokage, newHokage - 841422237
-            bot.sendMessage(msg.from.id, "Спасибо, наш менеджер скоро с вами свяжется:)");
-            user.splice(i, 1);
-            break;
-        } else {
-            bot.sendMessage(msg.from.id, "К сожалению вы нам не подходите, попробуйте в следующий раз.");
-            user.splice(i, 1);
-            break;
-        }
-    }
 }
